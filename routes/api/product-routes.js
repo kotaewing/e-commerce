46606e641a5c6dc88d051f1c451e9bfb945ee4f8
kotaewing <kota.ewing@gmail.com>
2021-06-14 13:49:23 -0600

    working on crud routes for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [{ Category }, { Tag }]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{ Category }, { Tag }]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Category and Tag data
 });
 
