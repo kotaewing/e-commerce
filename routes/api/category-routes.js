7f57f75467e4eab7f3d66bad160345148d573c39
kotaewing <kota.ewing@gmail.com>
2021-08-21 11:16:28 -0600

    user can add a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,6 +40,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
