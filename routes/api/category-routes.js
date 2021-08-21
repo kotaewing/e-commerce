407d6edf753d43d255fbe5b15d4506942972cfa1
kotaewing <kota.ewing@gmail.com>
2021-08-21 11:11:38 -0600

    user can get all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,17 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock']
+    }
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Products
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('sequelize');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+  app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
   });
 });
